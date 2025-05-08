564b2a47cb02eb34a0aad2d47a0c71f499e4f72b
Fabian Amino <aminofab@gmail.com>
2025-05-08 10:45:52 +0300

    added

---
+++ app/success/SuccessContent.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+
+export default function SuccessContent() {
+  const searchParams = useSearchParams()
+  
+  // Your existing success page logic here
+  return (
+    <div>
+      {/* Your success page content */}
+    </div>
+  )
+} 
