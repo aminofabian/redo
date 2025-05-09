74fb354fa5fb776cf3a4323b9064368e27b9f9ae
Fabian Amino <aminofab@gmail.com>
2025-05-09 13:49:31 +0300

    added

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   images: {
     domains: [
       'alexawriters.s3.eu-north-1.amazonaws.com',
+      'images.unsplash.com',
+      
       // Keep any existing domains here
     ],
   },
@@ -13,6 +15,20 @@ const nextConfig = {
       delete config.resolve.alias['next-auth'];
     }
     return config;
+  },
+  // Add headers configuration for CSP
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; img-src 'self' https://*.stripe.com https://alexawriters.s3.eu-north-1.amazonaws.com data:;"
+          }
+        ]
+      }
+    ];
   }
 };
 
